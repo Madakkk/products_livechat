a6b0c3244f9753cf8e65534d7286949ba7338d36
Madakkk <115184006+Madakkk@users.noreply.github.com>
2026-06-18 11:45:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
 app.get("/plans", (req, res) => {
   res.json({
     products: [
@@ -30,50 +35,7 @@ app.get("/plans", (req, res) => {
       }
     ]
   });
-});const express = require("express");
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({
-    status: "ok",
-    message: "Text Plans API"
-  });
 });
-
-app.get("/plans", (req, res) => {
-  res.json({
-    products: [
-      {
-        regular_price: "99",
-        title: "Growth",
-        price: "79",
-        id: "growth",
-        currency: "USD",
-        url: "https://www.text.com/pricing/",
-      },
-      {
-        regular_price: "25",
-        title: "Essential",
-        price: "19",
-        id: "essential",
-        currency: "USD",
-        url: "https://www.text.com/pricing/",
-      },
-      {
-        regular_price: "",
-        title: "Enterprise",
-        price: "Custom",
-        id: "enterprise",
-        currency: "USD",
-        url: "https://www.text.com/pricing/",
-      }
-    ]
-  });
-});
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
