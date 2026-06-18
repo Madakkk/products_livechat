29af7425247b82323bc990e1678631346c7b058b
Madakkk <115184006+Madakkk@users.noreply.github.com>
2026-06-18 11:40:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,36 @@
-const express = require("express");
+app.get("/plans", (req, res) => {
+  res.json({
+    products: [
+      {
+        regular_price: "25",
+        title: "Essential",
+        price: "19",
+        id: "essential",
+        currency: "USD",
+        url: "https://www.text.com/pricing/",
+        image_url: "https://via.placeholder.com/400x300?text=Essential"
+      },
+      {
+        regular_price: "99",
+        title: "Growth",
+        price: "79",
+        id: "growth",
+        currency: "USD",
+        url: "https://www.text.com/pricing/",
+        image_url: "https://via.placeholder.com/400x300?text=Growth"
+      },
+      {
+        regular_price: "custom",
+        title: "Enterprise",
+        price: "Custom offer",
+        id: "enterprise",
+        currency: "USD",
+        url: "https://www.text.com/pricing/",
+        image_url: "https://via.placeholder.com/400x300?text=Enterprise"
+      }
+    ]
+  });
+});const express = require("express");
 
 const app = express();
 
